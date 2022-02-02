aa93b5ed71e2753e74b34335fcf233721f696417
“gustavodornelas” <dornelas.gustavoptc@gmail.com>
2022-02-02 12:06:35 -0300

    First commit and First Class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import Boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
